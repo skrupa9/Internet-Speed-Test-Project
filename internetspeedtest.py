f5539d7205aca8add2b44a8d7c645dcc271d2b6e
szymon <skrupa@student.agh.edu.pl>
2022-07-12 17:45:38 +0200

    data is sending by google form

--- internetspeedtest.py
+++ internetspeedtest.py
@@ -1,7 +1,6 @@
 import time
 from datetime import date
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import paths
 
@@ -14,7 +13,7 @@ class InternetSpeedTest:
         self.download_speed = "-"
         self.upload_speed = "-"
         self.result_id = "-"
-        self.today = date.today()
+        self.today = str(date.today())
 
     def get_internet_speed(self):
         time.sleep(3)
@@ -30,10 +29,27 @@ class InternetSpeedTest:
 
         measure_id = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/'
                                                         'div[3]/div/div[3]/div/div/div[1]/div/div/div[2]/div[2]/a')
-        self.download_speed = download.text
-        self.upload_speed = upload.text
-        self.result_id = measure_id.text
+        self.download_speed = str(download.text)
+        self.upload_speed = str(upload.text)
+        self.result_id = str(measure_id.text)
 
     def save_results(self):
         self.driver.get(paths.FORM_LINK)
+        date_field = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/'
+                                                        'div[1]/div/div[1]/input')
+        download_field = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/'
+                                                            'div/div[1]/div/div[1]/input')
+        upload_field = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/'
+                                                          'div/div[1]/div/div[1]/input')
+        id_field = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/'
+                                                        'div[1]/div/div[1]/input')
+        submit_btn = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/'
+                                                        'span/span')
+        date_field.send_keys(self.today)
+        download_field.send_keys(self.download_speed)
+        upload_field.send_keys(self.upload_speed)
+        id_field.send_keys(self.result_id)
+        submit_btn.click()
+        self.driver.quit()
+
 
--- paths.py
+++ paths.py
@@ -1,3 +1,3 @@
 DRIVER_PATH = "C:\ChromeDriver/chromedriver.exe"
 SPEED_TEST_URL = "https://www.speedtest.net/pl"
-FORM_LINK = "https://forms.gle/ZN6eZDaCNLpgxhT47"
+FORM_LINK = "https://forms.gle/9uxaXhnLwDySxpWT6"
